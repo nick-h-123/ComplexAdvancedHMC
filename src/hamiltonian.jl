e9cf0ceecd3621420e8376c0e98264031d390c2d
nick-h-123 <77140192+nick-h-123@users.noreply.github.com>
2021-03-19 17:20:13 -0700

    Update hamiltonian.jl

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -26,7 +26,7 @@ end
 
 ∂H∂r(h::Hamiltonian{<:UnitEuclideanMetric}, r::AbstractVecOrMat) = copy(r)
 ∂H∂r(h::Hamiltonian{<:EuclideanMetric}, r::AbstractVecOrMat) = h.metric.M⁻¹ .* r
-∂H∂r(h::Hamiltonian{<:HermitianMetric}, r::AbstractVecOrMat) = h.metric.M⁻¹ * r
+∂H∂r(h::Hamiltonian{<:HermitianMetric}, r::AbstractVecOrMat) = h.metric.M⁻¹ .* r
 
 struct PhasePoint{T<:AbstractVecOrMat{<:Union{AbstractFloat, Complex}}, V<:DualValue}
     θ::T  # Position variables / model parameters.
